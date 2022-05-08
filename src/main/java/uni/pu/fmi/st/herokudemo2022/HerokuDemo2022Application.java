e9ab8fc94d3f65a4f73cc31753151a260e53b126
pepo-malinov <pepo.malinov@gmail.com>
2022-05-08 13:20:18 +0300

    init project

---
+++ src/main/java/uni/pu/fmi/st/herokudemo2022/HerokuDemo2022Application.java
@@ -0,0 +1,13 @@
+package uni.pu.fmi.st.herokudemo2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDemo2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDemo2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/uni/pu/fmi/st/herokudemo2022/HerokuDemo2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.pu.fmi.st.herokudemo2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemo2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
